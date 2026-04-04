1a835eb6682a9a093f3871133465de30971574f8
Dhruv <dhruvk2028@gmail.com>
2026-04-04 11:11:40 +0530

    Commit Hash fix for same Commit Message

--- src/fileUtils/hash.cpp
+++ src/fileUtils/hash.cpp
@@ -4,12 +4,22 @@
 #include <sstream>
 #include <iomanip>
 #include <string>
+#include <random>
 namespace hash{
   std::string sha256(const std::string& input) {
+    const std::string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(0, charset.size()-1);
+
+    std::string current = input;
+    for(int i=0; i<64; i++){
+      current += charset[dist(gen)];
+    }
     unsigned char hash[SHA256_DIGEST_LENGTH];
 
-    SHA256(reinterpret_cast<const unsigned char*>(input.c_str()),
-           input.size(),
+    SHA256(reinterpret_cast<const unsigned char*>(current.c_str()),
+           current.size(),
            hash);
 
     std::stringstream ss;
